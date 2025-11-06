1a1537f2bc1f495e0bdece860980b674efb1965b
brian <kimchanjoong54@gmail.com>
2025-11-06 16:15:30 +0900

    modify: AWS, S3 버킷 관련 세팅 추가

--- project/config/settings.py
+++ project/config/settings.py
@@ -267,3 +267,15 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',), # 인증 헤더 타입 (Bearer <token>)
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION', # 인증 헤더 이름
 }
+
+###AWS###
+AWS_ACCESS_KEY_ID = get_secret("AWS_ACCESS_KEY_ID") # .csv 파일에 있는 내용을 입력 Access key ID. IAM 계정 관련
+AWS_SECRET_ACCESS_KEY = get_secret("AWS_SECRET_ACCESS_KEY") # .csv 파일에 있는 내용을 입력 Secret access key. IAM 계정 관련
+AWS_REGION = 'ap-northeast-2'
+
+###S3###
+AWS_STORAGE_BUCKET_NAME = get_secret("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}

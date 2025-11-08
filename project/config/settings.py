a6c308e4c035fb0e22e580f878dad7e46c28dfb8
brian <kimchanjoong54@gmail.com>
2025-11-08 20:11:23 +0900

    feat: pymysql 초기화 코드 추가

--- project/config/settings.py
+++ project/config/settings.py
@@ -3,6 +3,9 @@ import os, json
 from django.core.exceptions import ImproperlyConfigured
 from datetime import timedelta
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

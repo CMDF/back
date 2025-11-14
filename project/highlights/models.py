a4260c5ec430ce8bfca12d357e8257287206e12b
brian <kimchanjoong54@gmail.com>
2025-11-14 12:48:32 +0900

    feat: Highlight 정보를 담는 table 구현

--- project/highlights/models.py
+++ project/highlights/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from pdf_documents.models import originPDF
+from pdf_documents.models import originPDF, PDFpage
 
 class Tag(models.Model):
     pdf_id = models.ForeignKey(originPDF, on_delete=models.CASCADE)
@@ -8,3 +8,10 @@ class Tag(models.Model):
 
     def __str__(self):
         return f"{self.tag_detail}: {self.color}"
+    
+class Highlight(models.Model):
+    pdf_id = models.ForeignKey(originPDF, on_delete=models.CASCADE)
+    page_id = models.ForeignKey(PDFpage, on_delete=models.CASCADE)
+    Tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    highlight_text = models.TextField()
+    highlight_box = models.JSONField()  # { "min_x": ~, "min_y": ~, "max_x": ~, "max_y": ~  }

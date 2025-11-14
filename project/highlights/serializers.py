8a8ea4d437a465facac3e20783befde2dfcf76c7
brian <kimchanjoong54@gmail.com>
2025-11-14 13:04:39 +0900

    feat: highlight 직렬화 코드 추가

--- project/highlights/serializers.py
+++ project/highlights/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Tag
+from .models import Tag, Highlight
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'pdf_id', 'color', 'tag_detail']
         read_only_fields = ['id']
+
+class HighlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Highlight
+        fields = ['id', 'page_id', 'start_index', 'Tag_id', 'highlight_text', 'highlight_box']
+        read_only_fields = ['id', 'created_at']

848f2232f0a821e48c16d1ce039a1cdacc5d7be1
brian <kimchanjoong54@gmail.com>
2025-11-21 21:45:45 +0900

    feat: PDF id를 파라미터로 입력 받으면 해당 pdf에 연결된 모든 figures 데이터를 가져오는 기능, url 구현

--- project/pdf_figures/views.py
+++ project/pdf_figures/views.py
@@ -1,1 +1,23 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import PDFfigure
+from .serializers import PDFfigureSerializer
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+class GetFiguresByOriginPDFAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_id="getFiguresByOriginPDF",
+        operation_description="주어진 PDF 문서 ID에 연결된 모든 PDF figure를 조회합니다.",
+        tags=["PDF Figures"],
+        responses={200: PDFfigureSerializer(many=True), 401: openapi.Response("Unauthorized")},
+    )
+    def get(self, request, pdf_id):
+        qs = PDFfigure.objects.filter(page_id__pdf_id=pdf_id)
+        return Response(PDFfigureSerializer(qs, many=True).data, status=status.HTTP_200_OK)
